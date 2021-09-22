6b204a294993e83513b1edbaeb8a0a5489492f01
밈민찬 <minchan1472@naver.com>
2021-09-22 19:21:46 +0900

    add extension folder

---
+++ extension/test_0922/popup.js
@@ -0,0 +1,29 @@
+
+function extraction(e){
+
+    chrome.tabs.executeScript({code:"document.location.href;"},url_extraction);
+
+    chrome.tabs.executeScript({code:"document.querySelector('#articleTitle').innerText;"},naver_title_extraction);
+    // 익스텐션 창이아닌 크롬 창의 필드에서 타이틀 획득 후 naver_title_extraction함수로 전달/호출
+    chrome.tabs.executeScript({code:"document.querySelector('#articleBodyContents').innerText;"},naver_contents_extraction);
+    // 익스텐션 창이아닌 크롬 창의 필드에서 기사 획득 후 naver_contents_extraction 전달/호출
+}
+
+function url_extraction(results){
+    document.querySelector('#url').value=results;
+}
+
+function naver_title_extraction(results){
+    document.querySelector('#title').value=results;
+}
+
+function naver_contents_extraction(results){
+    document.querySelector('#contents').value=results;
+}
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    var btn01 = document.querySelector('#btn');
+    btn01.addEventListener("click",extraction); // 기사획득 버튼 누르면 실행
+
+});
